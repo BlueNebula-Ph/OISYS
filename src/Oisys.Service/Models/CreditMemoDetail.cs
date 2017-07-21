8f8644a56676a17692ba440b2fc1234b8cade04f
KarenFeraer <feraer.karen@bcg.com>
2017-07-21 22:49:06 +0800

    Completed on model creating, setup swagger and customercontroller WIP

--- src/Oisys.Service/Models/CreditMemoDetail.cs
+++ src/Oisys.Service/Models/CreditMemoDetail.cs
@@ -1,11 +1,16 @@
 ﻿namespace Oisys.Service.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class CreditMemoDetail : ModelBase
     {
+        [Required]
         public int CreditMemoId { get; set; }
 
+        [Required]
         public int ItemId { get; set; }
 
+        [Required]
         public decimal Quantity { get; set; }
 
         public Item Item { get; set; }
--- src/Oisys.Service/Models/OrderDetail.cs
+++ src/Oisys.Service/Models/OrderDetail.cs
@@ -1,13 +1,21 @@
 ﻿namespace Oisys.Service.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class OrderDetail : ModelBase
     {
+        [Required]
         public int OrderId { get; set; }
 
+        [Required]
         public decimal Quantity { get; set; }
 
+        [Required]
         public int ItemId { get; set; }
 
+        [Required]
         public decimal Price { get; set; }
+
+        public Order Order { get; set; }
     }
 }
