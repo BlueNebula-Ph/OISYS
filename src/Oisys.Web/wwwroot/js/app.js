925db6277a1a1c4725e081078ec6dda927a5f13a
VonAerich <von.aerich@gmail.com>
2017-08-06 22:01:07 +0800

    Updated to use ui-router.

--- src/Oisys.Web/wwwroot/js/app.js
+++ src/Oisys.Web/wwwroot/js/app.js
@@ -8,55 +8,70 @@
         Object.assign(env, window._environment);
     };
 
-    angular.module("oisys-app", ["ngRoute"])
+    angular.module("oisys-app", ["ui.router"])
         .constant("env", env)
-        .config(["$routeProvider", "$locationProvider", function ($routeProvider, $locationProvider) {
-            $routeProvider
-                .when("/", {
+        .config(["$stateProvider", "$urlRouterProvider", function ($stateProvider, $urlRouterProvider) {
+            //$routeProvider
+            //    .when("/", {
+            //        templateUrl: "/views/home/index.html"
+            //    }).when("/login", {
+            //        templateUrl: "/views/common/login.html",
+            //        controller: "loginController",
+            //        controllerAs: "ctrl"
+            //    }).when("/list/customers", { // Customer routes
+            //        templateUrl: "/views/customer/index.html",
+            //        controller: "viewCustomerController",
+            //        controllerAs: "ctrl"
+            //    }).when("/add/customer/:id", {
+            //        templateUrl: "/views/customer/add-customer.html",
+            //        controller: "addCustomerController",
+            //        controllerAs: "ctrl"
+            //    }).when("/details/customer/:id", {
+            //        templateUrl: "/views/customer/customer-details.html",
+            //        controller: "customerDetailsController",
+            //        controllerAs: "ctrl"
+            //    }).when("/list/orders", { // Orders route
+            //        templateUrl: "/views/order/index.html",
+            //        controller: "",
+            //        controllerAs: ""
+            //    }).when("/list/inventory", { // Inventory route
+            //        templateUrl: "/views/inventory/index.html",
+            //        controller: "",
+            //        controllerAs: ""
+            //    }).when("/list/deliveries", { // Deliveries route
+            //        templateUrl: "/views/delivery/index.html",
+            //        controller: "",
+            //        controllerAs: ""
+            //    }).when("/list/users", { // Users route
+            //        templateUrl: "/views/user/index.html",
+            //        controller: "",
+            //        controllerAs: ""
+            //    }).when("/references", { // References route
+            //        templateUrl: "/views/reference/index.html",
+            //        controller: "",
+            //        controllerAs: ""
+            //    }).otherwise({ // Otherwise, route to home
+            //        redirectTo: "/"
+            //    });
+
+            //$locationProvider.html5Mode({
+            //    enabled: false,
+            //    requireBase: false
+            //});
+
+            $urlRouterProvider.otherwise("/home");
+
+            $stateProvider
+                .state("home", {
+                    url: "/home",
                     templateUrl: "/views/home/index.html"
-                }).when("/login", {
-                    templateUrl: "/views/common/login.html",
-                    controller: "loginController",
-                    controllerAs: "ctrl"
-                }).when("/list/customers", { // Customer routes
+                })
+
+                .state("customers", {
+                    url: "/manage/customers",
                     templateUrl: "/views/customer/index.html",
                     controller: "viewCustomerController",
                     controllerAs: "ctrl"
-                }).when("/add/customer/:id", {
-                    templateUrl: "/views/customer/add-customer.html",
-                    controller: "addCustomerController",
-                    controllerAs: "ctrl"
-                }).when("/details/customer/:id", {
-                    templateUrl: "/views/customer/customer-details.html",
-                    controller: "customerDetailsController",
-                    controllerAs: "ctrl"
-                }).when("/list/orders", { // Orders route
-                    templateUrl: "/views/order/index.html",
-                    controller: "",
-                    controllerAs: ""
-                }).when("/list/inventory", { // Inventory route
-                    templateUrl: "/views/inventory/index.html",
-                    controller: "",
-                    controllerAs: ""
-                }).when("/list/deliveries", { // Deliveries route
-                    templateUrl: "/views/delivery/index.html",
-                    controller: "",
-                    controllerAs: ""
-                }).when("/list/users", { // Users route
-                    templateUrl: "/views/user/index.html",
-                    controller: "",
-                    controllerAs: ""
-                }).when("/references", { // References route
-                    templateUrl: "/views/reference/index.html",
-                    controller: "",
-                    controllerAs: ""
-                }).otherwise({ // Otherwise, route to home
-                    redirectTo: "/"
                 });
-
-            $locationProvider.html5Mode({
-                enabled: false,
-                requireBase: false
-            });
         }]);
 })();
