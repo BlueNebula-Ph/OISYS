9072c72eca142df6096ba588533173c1958d8de9
Von Tan <vont@magenic.com>
2017-08-08 00:04:40 +0800

    Added services js

--- src/Oisys.Web/wwwroot/js/services/item-service.js
+++ src/Oisys.Web/wwwroot/js/services/item-service.js
@@ -1,34 +1,31 @@
 ﻿(function (module) {
 
     var customerService = function ($http, env) {
-        var urlBase = env.baseUrl + "/api/customer";
+        var urlBase = env.baseUrl + "/api/item";
         var dataFactory = {};
 
-        dataFactory.fetchAllCustomers = function () {
-        };
-
-        dataFactory.fetchCustomers = function (page, sortBy, sortDirection, search) {
+        dataFactory.fetchItems = function (page, sortBy, sortDirection, search) {
             var url = urlBase + "/search";
             var filter = { sortBy: sortBy, sortDirection: sortDirection };
 
             return $http.post(url, filter);
         };
 
-        dataFactory.getCustomer = function (id) {
+        dataFactory.getItem = function (id) {
             var url = urlBase + "/" + id;
             return $http.get(url);
         };
 
-        dataFactory.saveCustomer = function (id, customer) {
+        dataFactory.saveItem = function (id, item) {
             if (id === 0) {
-                return $http.post(urlBase, customer);
+                return $http.post(urlBase, item);
             } else {
                 var url = urlBase + "/" + id;
-                return $http.put(url, customer);
+                return $http.put(url, item);
             }
         };
 
-        dataFactory.deleteCustomer = function (id) {
+        dataFactory.deleteItem = function (id) {
             var url = urlBase + "/" + id;
             return $http.delete(url);
         };
