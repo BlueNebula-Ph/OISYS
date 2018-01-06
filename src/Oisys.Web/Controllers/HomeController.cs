220a1ff6607db37849c77509b067ff1485044904
VonAerich <von.aerich@gmail.com>
2018-01-06 17:10:18 +0800

    Oisys Cleanup

--- src/Oisys.Web/Controllers/HomeController.cs
+++ src/Oisys.Web/Controllers/HomeController.cs
@@ -1,15 +1,17 @@
-﻿namespace Oisys.Web.Controllers
+namespace Oisys.Service.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
 
+    /// <summary>
+    /// The main entry point from the user interface.
+    /// </summary>
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Serves the index page.
+        /// </summary>
+        /// <returns>The index view.</returns>
         public IActionResult Index()
-        {
-            return this.View();
-        }
-
-        public IActionResult Error()
         {
             return this.View();
         }
--- src/Oisys.Web/Program.cs
+++ src/Oisys.Web/Program.cs
@@ -1,14 +1,18 @@
-﻿namespace Oisys.Web
+﻿namespace Oisys.Service
 {
-    using System;
-    using System.Collections.Generic;
     using System.IO;
-    using System.Linq;
-    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
 
+    /// <summary>
+    /// The program class
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// The main method called on startup
+        /// </summary>
+        /// <param name="args">Initial arguments</param>
         public static void Main(string[] args)
         {
             var host = new WebHostBuilder()
