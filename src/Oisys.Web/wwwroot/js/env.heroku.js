8ba98337fe845d3c9027ce1dc8e408da4873e7cd
VonAerich <von.aerich@gmail.com>
2018-02-12 01:50:57 +0800

    Added heroku stuff for staging

---
+++ src/Oisys.Web/wwwroot/js/env.heroku.js
@@ -0,0 +1,10 @@
+﻿(function (window) {
+    window._environment = window.__env || {};
+
+    // API url
+    window._environment.baseUrl = 'https://oisys.herokuapp.com';
+
+    // Whether or not to enable debug mode
+    // Setting this to false will disable console output
+    window._environment.enableDebug = false;
+}(this));
